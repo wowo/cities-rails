950a3ca852f7284c5c96cc27f35835d7bb84b161
Wojciech Sznapka <wojciech@sznapka.pl>
2012-01-16 21:14:43 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitiesRails::Application.config.secret_token = '8544f292062cc605f7e995037230ac22c908475d5a0a72106f74c58b0bee6eefb50d812a3fd274185c7a1368d81730a7950a7f34894f6aa8917d733577ab0cd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CitiesRails::Application.config.session_store :cookie_store, :key => '_cities-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CitiesRails::Application.config.session_store :active_record_store
